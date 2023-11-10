6d8f0b901723a8e892fc8ba67db53d96b4047224
AbelLouis7 <abellouisfernandez@gmail.com>
2023-11-10 22:01:33 +0530

    Update settings.py

--- suggestforme1/suggestforme1/settings.py
+++ suggestforme1/suggestforme1/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'HOST': os.getenv('MYSQLHOST'),  
         'PORT': os.getenv('MYSQLPORT'),  
         'OPTIONS': {
-            'charset': 'utf8mb4',
+            'charset': 'utf8',
         },    
     }
 }
