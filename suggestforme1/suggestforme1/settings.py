6517dc69ebb76f2ae87a61d95d57e6aded74d270
AbelLouis7 <abellouisfernandez@gmail.com>
2023-11-10 22:29:21 +0530

    22:30

--- suggestforme1/suggestforme1/settings.py
+++ suggestforme1/suggestforme1/settings.py
@@ -130,10 +130,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
